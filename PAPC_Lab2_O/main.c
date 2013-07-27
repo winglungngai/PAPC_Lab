5e52702992b45c4992ef2da766162fb282c4bafa
Wing Lung Ngai <w.l.ngai@student.tudelft.nl>
2013-07-27 02:01:00 +0200

    reformat labwork2

--- PAPC_Lab2_O/main.c
+++ PAPC_Lab2_O/main.c
@@ -6,85 +6,142 @@
 void merge(int *A, int *B, int *AB, int n, int m, int threads_num);
 int rank(int a, int *sortedArray, int startPos, int endPos);
 int compare_ints (const void *a, const void *b);
+void mergeSeq_r(int *A, int *B, int *AB, int n, int m, int aPos, int bPos, int abPos);
+void setup(int **A, int **B, int **AB, int n, int m);
+void printDataset(char * description, int * dataset, int size);
+void timeFunc(void (*f)(int *, int *, int *, int, int, int), int *A, int *B, int *AB, int n, int m, int threads_num, int repeat);
+void mergeSeq(int *A, int *B, int *AB, int n, int m, int threads_num);
 
 main ()
 {
-    int h,i,j,k,l;
+    int h,i,j,k;
+    int n_var=7;
+    int t_var=5;
     int nSize[7] = {16, 64, 256, 1024, 4096, 16384, 65536};
     int threads[5] = {1, 2, 4, 8, 16};
     int repeat = 1000;
 
-    printf("\t\t");
+    printf("\t\tseq\t");
     for(h=0;h<5;h++)
-    {
         printf("t=%i\t", threads[h]);
-    }
     printf("\n");
 
-    for(i=0; i<7; i++)
+    for(i=0; i<n_var; i++)
     {
-        int m,n;
         //set up inputs
-        n = nSize[i];
-        m = nSize[i]*2;
-        int A[n], B[m], AB[n+m];
+        int *A, *B, *AB;
+        int n = nSize[i]*2/3;
+        int m = nSize[i]/3;
+        setup(&A, &B, &AB, n, m);
 
-        srand (time(NULL));
-        for(j=0; j<n; j++)
-            A[i]=rand() % 30;
-        for(j=0; j<m; j++)
-            B[i]=rand() % 30;
-
-        qsort (A, n, sizeof (int), compare_ints);
-        qsort (B, m, sizeof (int), compare_ints);
-
-        //show inputs
-    //    printf("A: ");
-    //    for(i=0; i<n; i++)
-    //        printf("%i ", A[i]);
-    //    printf("\n");
-    //
-    //    printf("B: ");
-    //    for(i=0; i<m; i++)
-    //        printf("%i ", B[i]);
-    //    printf("\n");
+        //printDataset("Input A: ", A, n);
+        //printDataset("Input B: ", B, m);
 
         printf("size=%.5i\t", nSize[i]);
-        for(k=0;k<5;k++)
+
+        timeFunc(mergeSeq, A, B, AB, n, m, 1, repeat);
+
+        for(k=0;k<t_var;k++)
         {
             //set up threads number.
             int threads_num = threads[k];
             omp_set_dynamic(0);
             omp_set_num_threads(threads_num);
 
-            //start time measurement
-            struct timeval startt, endt, result;
-            int t;
-            int TIMES = 1000000;
-            result.tv_usec=0;
-            gettimeofday (&startt, NULL);
-
-            //run parellel algorithm
-            for(l=0; l<repeat;l++)
-                merge(A, B, AB, n, m, threads_num);
-
-            //end time measurement
-            gettimeofday (&endt, NULL);
-            result.tv_usec = (endt.tv_sec*1000000+endt.tv_usec) - (startt.tv_sec*1000000+startt.tv_usec);
-            long timeDiff = result.tv_usec/repeat;
-
-            //show elapsed time.
-            printf("%03ld\t", timeDiff);
+            timeFunc(merge, A, B, AB, n, m, threads_num, repeat);
         }
         printf("\n");
 
-        //show outputs
-    //    printf("AB: ");
-    //    for(i=0; i<m+n; i++)
-    //        printf("%i ", AB[i]);
-    //    printf("\n");
+        //printDataset("Output AB: ", AB, n+m);
     }
 }
+
+void timeFunc(void (*f)(int *, int *, int *, int, int, int), int *A, int *B, int *AB, int n, int m, int threads_num, int repeat)
+{
+    //start time measurement
+    struct timeval startt, endt, result;
+    int t;
+    long timeDiff;
+    result.tv_usec=0;
+    gettimeofday (&startt, NULL);
+
+    int l;
+    for(l=0; l<repeat;l++)
+        (*f)(A, B, AB, n, m, threads_num);
+
+    //end time measurement
+    gettimeofday (&endt, NULL);
+    result.tv_usec = (endt.tv_sec*1000000+endt.tv_usec) - (startt.tv_sec*1000000+startt.tv_usec);
+    timeDiff = result.tv_usec/repeat;
+
+    //show elapsed time.
+    printf("%03ld\t", timeDiff);
+}
+
+void printDataset(char * description, int * dataset, int size)
+{
+    int i;
+    printf(description);
+    for(i=0; i<size; i++)
+        printf("%i ", dataset[i]);
+    printf("\n");
+}
+
+void setup(int **A, int **B, int **AB, int n, int m)
+{
+    int j;
+
+    *A = (int *) malloc(n*sizeof(int));
+    *B = (int *) malloc(m*sizeof(int));
+    *AB = (int *) malloc((n+m)*sizeof(int));
+
+    srand (time(NULL));
+    for(j=0; j<n; j++)
+        (*A)[j]=rand() % 30;
+    for(j=0; j<m; j++)
+        (*B)[j]=rand() % 30;
+
+    qsort (*A, n, sizeof (int), compare_ints);
+    qsort (*B, m, sizeof (int), compare_ints);
+}
+
+void mergeSeq(int *A, int *B, int *AB, int n, int m, int threads_num)
+{
+    mergeSeq_r(A, B, AB, n, m, 0, 0, 0);
+}
+
+void mergeSeq_r(int *A, int *B, int *AB, int n, int m, int aPos, int bPos, int abPos)
+{
+    if(abPos < n+m)
+    {
+        if(aPos==n)
+        {
+            AB[abPos]=B[bPos];
+            bPos++;
+        }
+        else if(bPos==m)
+        {
+            AB[abPos]=A[aPos];
+            aPos++;
+        }
+        else
+        {
+            if(A[aPos]<B[bPos])
+            {
+                AB[abPos]=A[aPos];
+                aPos++;
+            }
+            else
+            {
+                AB[abPos]=B[bPos];
+                bPos++;
+            }
+        }
+        abPos++;
+        mergeSeq_r(A, B, AB, n, m, aPos, bPos, abPos);
+    }
+}
+
 
 void merge(int *A, int *B, int *AB, int n, int m, int threads_num)
 {
